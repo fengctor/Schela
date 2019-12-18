52716fe6d2e96ccd970e3e0f853684a878ab36de
Gary Feng <gary.feng@esentire.com>
2019-12-18 14:57:45 -0500

    update pack

--- test.scm
+++ test.scm
@@ -7,3 +7,12 @@
                 ((if b (curry map (curry cons x)) id)
                  (filterM pred xs)))
               (pred x))]))
+
+;; strict Y combinator
+(define Y (lambda (f) ((lambda (x) (f (lambda (v) ((x x) v))))
+                       (lambda (x) (f (lambda (v) ((x x) v)))))))
+
+(define Fact
+  (Y (lambda (fact) (lambda (n) (if (zero? n) 1 (* n (fact (- n 1))))))))
+(define Fib
+  (Y (lambda (fib) (lambda (n) (if (<= n 1) n (+ (fib (- n 1)) (fib (- n 2))))))))
